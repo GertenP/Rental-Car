b16ce11b7ca18037a397eb97a04f208da51b2adb
GertenP <gerten.pilv@tptlive.ee>
2026-04-08 04:30:52 +0300

    Ülesanne 1: Refaktoreerimine + uued nõuded

--- index.js
+++ index.js
@@ -10,19 +10,20 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 app.use('/pictures', express.static('images'));
-
 const formHtml = fs.readFileSync('form.html', 'utf8');
 const resultHtml = fs.readFileSync('result.html', 'utf8');
 
 app.post('/', (req, res) => {
     const post = req.body;
+    // Siin toimub rentalPrice.js failis price funktsiooni kasutamine!
+    // PS! Kõikidel on ümber ka parsimine mingisse andmetüüpi, et oleks kindel, et funktsiooni õige läheb!
     const result = rental.price(
-        String(post.pickup),
-        String(post.dropoff),
-        Date.parse(post.pickupdate),
-        Date.parse(post.dropoffdate),
-        String(post.type),
-        Number(post.age)
+        String(post.pickup), // pickup aeg
+        String(post.dropoff), // dropoff aeg
+        Date.parse(post.pickupdate), //pickupDate
+        Date.parse(post.dropoffdate), //dropOffDate
+        String(post.type), // type
+        Number(post.age) // vanus
     );
     res.send(formHtml + resultHtml.replaceAll('$0', result));
 });
